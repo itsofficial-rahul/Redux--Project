2f5a43c4df4ef4d00f44ba6e3bc0aeb12fbe7f46
Rahul choudhary <rsc1401590@gmail.com>
2022-02-01 04:48:45 -0800

    css improve

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import React, { useState } from 'react';
-import {incriment,decriment} from"./Action/Action";
-import{useSelector,useDispatch} from"react-redux";
-function App()
-{
-  const mystate=useSelector((state)=>state.changeNumber)
-  const dispatch=useDispatch();
-  return(
+import { incriment, decriment } from "./Action/Action";
+import { useSelector, useDispatch } from "react-redux";
+function App() {
+  const mystate = useSelector((state) => state.changeNumber)
+  const dispatch = useDispatch();
+  return (
     <div>
-      <h1>React with redux </h1>
+      <h1 style={{color:"red",backgroundColor:"blue",width:"250px",margin:"60px"}}>React with redux </h1>
       <div className='btn'>
-        <button onClick={()=>dispatch(incriment())}>add</button>
-          <h1>{mystate}</h1>
-        <button onClick={()=>dispatch(decriment())}>remove</button>
+        <button onClick={() => dispatch(incriment())}>add</button>
+        <h1>{mystate} </h1>
+        <button onClick={() => dispatch(decriment())}>remove</button>
       </div>
     </div>
   )
